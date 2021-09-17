28792f673784117a6bfb361210dc38f9394c3682
spostew <spencerunitt0@gmail.com>
2021-09-17 14:29:52 -0700

    Added Lab_2 and all needed comments

--- Lab_1/MainWindow.xaml.cs
+++ Lab_1/MainWindow.xaml.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿/*
+    Lab_2: This program will take a standard name and convert it to pig latin
+    Name: Spencer Unitt
+    Module: Lab_2, Part1
+    Problem Statement: Use pig latin to alter your first and last name
+    Algorithm:
+        1. Create two strings named first and last and set them = to your name
+        2. Move the first char from the first string and last string to the back
+        3. In a new string named full, concatinate the strings together and set the
+           first char to uppercase
+        4. Write the result to the console.
+        
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -25,27 +39,27 @@ namespace Lab_1
             InitializeComponent();
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
+  
 
         private void pigLatinButton_Click(object sender, RoutedEventArgs e)
         {
             // Returns the data from the textboxes 
             String first_name = firstNameTextBox.Text,
                    last_name = lastNameTextbox.Text, 
-                   pig_First, pig_Last;
+                   pig_First, pig_Last, pig = "ay";
             // Sets the strings to lowercase
             first_name = first_name.ToLower();
             last_name = last_name.ToLower();
 
-            pig_First = first_name.Substring(1, first_name.Length - 1) + first_name.Substring(0, 1) + "ay";
+            // Takes in the first name and rearranges the letters while also adding "ay" to the end
+            pig_First = first_name.Substring(1, first_name.Length - 1) + first_name.Substring(0, 1) + pig;
             pig_First = pig_First.Substring(0, 1).ToUpper() + pig_First.Substring(1, pig_First.Length - 1);
 
-            pig_Last = last_name.Substring(1, last_name.Length - 1) + last_name.Substring(0, 1) + "ay";
+            // Does the same thing as the first name but to the last name instead
+            pig_Last = last_name.Substring(1, last_name.Length - 1) + last_name.Substring(0, 1) + pig;
             pig_Last = pig_Last.Substring(0, 1).ToUpper() + pig_Last.Substring(1, pig_Last.Length - 1);
 
+            // Displays the results in a messagebox
             MessageBox.Show("Your name in piglatin is: " + pig_First + " " + pig_Last);
 
         }
---
+++ Lab_2/Program.cs
@@ -0,0 +1,80 @@
+﻿/*
+ * Lab_2: This program will ask the user a series of questions to determine if they need a new lamp. For each answer the user gives
+ *        the program will return a new question until they reach the end
+ * Name: Spencer Unitt
+ * Module: Week_4_Labs, Lab_2
+ * 
+ * Algorithm:
+ *  1. Show a welcome screen explained to user what the program will do.
+ *  2. Ask the user if they need a new lamp
+ *  3. Prompt the user for yes or no answer with Y or N
+ *  4. If user types N then tell them to plug in the lamp and exit program
+ *  5. Otherwise if user types Y then ask if the bulb is burned out
+ *  6. If the user enters N again then say it's time to get a new lamp and exit program
+ *  7. Otherwise if user types Y then tell them to replace the bulb and exit the program
+ *  
+ *  If the user enters anything other than "Y" or "N" throw invalid reponse error and exit program*  
+ * 
+ */ 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string selection;
+
+            // Displays what the program does to the user and prompts for response.
+            Console.WriteLine("I'll determine if you need a new lamp or not.");
+            Console.WriteLine("Type N for no or Y for yes");
+            Console.WriteLine("So your lamp doesn't work? Did you plug it in?");
+            selection = Console.ReadLine();
+            // If the user types a lower y or n by mistake this will convert it to uppercase
+            selection = selection.ToUpper();
+
+            
+            if(selection == "Y")
+            {
+                // If the selection is Y this ask a follow up question
+                Console.WriteLine("Is the Lightbulb burned out?");
+                selection = Console.ReadLine();
+                selection = selection.ToUpper();
+
+                // If the user enters Y or N then display the results in the code block and exit the program
+                // The read line will wait for the user to click elsewhere so the program can explain the next step
+                if(selection == "Y")
+                {
+                    Console.WriteLine("Try to replace the bulb first");
+                    Console.ReadLine();
+                }
+                else if(selection == "N")
+                {
+                    Console.WriteLine("Time to get a new lamp!");
+                    Console.ReadLine();
+                }
+
+            }
+            // If the result is false in the first if then check if value equals "N" 
+            else if(selection == "N")
+            {
+                Console.WriteLine("Try plugging in the lamp first and try again.");
+                Console.ReadLine();
+            }
+            // If "Y" or "N" is not entered then throw error and exit
+            else
+            {
+                Console.WriteLine("That input is not valid, try again.");
+                Console.ReadLine();
+            }
+
+
+        }
+    }
+}
